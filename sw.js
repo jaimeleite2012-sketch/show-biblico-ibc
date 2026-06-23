0a5d58231dda0d2d36fc7678e0dbd13fd81e7737
jaimeleite2012-sketch <jaimeleite2012@gmail.com>
2026-06-22 22:00:17 -0300

    Update cache version for V13.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'show-biblico-ibc-v13-1-20260622';
+const CACHE_NAME = 'show-biblico-ibc-v13-2-20260622';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -19,6 +19,21 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const requestUrl = new URL(event.request.url);
+
+  if (event.request.mode === 'navigate' || requestUrl.pathname.endsWith('/index.html')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
